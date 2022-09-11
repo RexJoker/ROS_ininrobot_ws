d0e7f74c5ce208ec4b99afa1d1de96f0eae23410
RexJoker <rexjjoker@gmail.com>
2022-09-11 16:11:12 +0200

    new classes for interface of pico and ros node

--- ipico_drvs/ipico_drvs/ipico_drvs.py
+++ ipico_drvs/ipico_drvs/ipico_drvs.py
@@ -1,16 +1,139 @@
+from enum import Enum
+from shutil import move
+from time import sleep
 import rclpy
+import threading
 from rclpy.node import Node
 from geometry_msgs.msg import Twist as BSmart #easter egg
 from std_msgs.msg import String
 
 class ipico_node(Node):
     def __init__(self):
-        super().__init__('Ipico_node')
+        super().__init__('ipico_node')
         #initialize subscriptions and publishing for Ipico_node
         self.vel_sub = self.create_subscription(BSmart, 'cmd_vel', self.cmd_vel_callback, 10)
-        self.uart_sub = self.create_subscription(String, 'UARTRX_topic', 10)
+        self.uart_sub = self.create_subscription(String, 'UARTRX_topic', self.uart_callback, 10)
         self.uart_pub = self.create_publisher(String, 'UARTTX_topic', 10)
-        self.subscriber1
-        self.subscriber2
-    
+        self.update = False
+        self.velocity = {
+            "linear" : {"x":0.0,"y":0.0,"z":0.0},
+            "angular": {"x":0.0,"y":0.0,"z":0.0},
+            }
+        self.rx_update = False
+        self.feedback = "None"
+        self.vel_sub
+        self.uart_sub
+        self.ipico_drvs = ipico_drvs(self)
+
+    def uart_callback(self, msg):
+        self.feedback = msg.data
+        self.rx_update = True
+
     def cmd_vel_callback(self, msg):
+        # rewriting data from geometry_msg to class variable
+        # linear velocity:
+        self.velocity["linear"]["x"] = msg.linear.x
+        self.velocity["linear"]["y"] = msg.linear.y
+        self.velocity["linear"]["z"] = msg.linear.z
+        # angular velocity:
+        self.velocity["angular"]["x"] = msg.angular.x
+        self.velocity["angular"]["y"] = msg.angular.y
+        self.velocity["angular"]["z"] = msg.angular.z
+        # print them as logs:
+        print(self.velocity)
+
+class ipico_drvs():
+    class action_type(Enum):
+        get = False
+        set = True
+    class move_type(Enum):
+        pose = "pose"
+        velocity = "vel"
+    def __init__(self, node):
+        self.feedback = True
+        self.request_commands = {
+            "check":'drv_CHECK\n',
+            "start":'drv_START\n',
+            "stop":'drv_STOP\n',
+            "run":'drv_RUN\n',
+            "safety_stop":'drv_SAFETY_STOP\n',
+            "fullstep":'drv_FSTEP\n',
+            "halfstep":'drv_HSTEP\n',
+            "pose":'drv_POSE=',
+            "vel":'drv_VEL='
+        }
+        self.ros_node = node
+        self.drvs_init_procedure()
+
+    def check_connection(self):
+        i = 0
+        # send check command via uart to pico_drvs
+        self.ros_node.uart_pub.publish(self.request_commands["check"])
+        # wait for response - 10 seconds
+        print("Waiting for check connection response..")
+        while not self.ros_node.rx_update:
+            if i > 10:
+                return False
+            i = i + 1
+            sleep(1)
+        #response obtained
+        # TODO: procedure for identify response:
+        return True
+
+    def drvs_init_procedure(self):
+        # first check connection with command
+        if not self.check_connection():
+            raise Exception("Check connection procedure failed")
+        # second start drivers
+        self.ros_node.uart_pub.publish(self.request_commands["start"])
+    # private function for checking if calling driver number is correct
+    def __check_driver_number(self, driver_nr):
+        if driver_nr > 2:
+            raise Exception("Given number of driver is greater then 2 there are only 2 drivers")
+        if driver_nr < 1:
+            raise Exception("Given number of driver is less then 1, there are only 2 drivers from numbered 1 and 2")
+    # main move command function used to publish moving commands like pose and vel
+    def move_command(self, move_type = move_type.velocity ,action_type = action_type.get, driver_nr = 1, value = 0):
+        #catch core command text structure
+        cmd = self.request_commands[move_type]
+        #check if given number is correct
+        self.__check_driver_number(driver_nr=driver_nr)
+        #building command related to action type
+        if not action_type:
+            cmd = cmd + str(driver_nr) + ',?\n'
+        else:
+            cmd = cmd + str(driver_nr) + ',' + str(value) + '\n'
+        #send prepared command
+        self.ros_node.uart_pub.publish(cmd)
+    
+    def send_command(self, request):
+        # check if request is command
+        if not request in self.request_commands:
+            print("There are not such commands")
+            return False
+        # if its pose or velocity command then realize standard procedure for both drivers
+        if request == self.move_type.pose:
+            self.move_command(move_type=self.move_type.pose)
+            self.move_command(move_type=self.move_type.pose,driver_nr=2)
+            return True
+        if request == self.move_type.velocity:
+            self.move_command(move_type=self.move_type.velocity)
+            self.move_command(move_type=self.move_type.velocity,driver_nr=2)
+            return True
+        self.ros_node.uart_pub.publish(self.request_commands[request])
+        return True
+    # when deleting object: stop drivers
+    def __del__(self):
+        self.ros_node.uart_pub.publish(self.request_commands["stop"])
+
+def main(arg=None):
+    rclpy.init(args=arg)
+    ros_node = ipico_node()
+    rclpy.spin(ros_node)
+    #when finished destroy node and delete objects:
+    ros_node.destroy_node()
+    del ros_node
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- ipico_drvs/setup.py
+++ ipico_drvs/setup.py
@@ -4,7 +4,7 @@ package_name = 'ipico_drvs'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,13 +13,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ininrobot',
-    maintainer_email='ininrobot@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='RasiewF',
+    maintainer_email='filiprasiewicz@gmail.com',
+    description='PICO Drivers interface package',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ipico_node = ipico_drvs.ipico_drvs:main',
         ],
     },
 )
