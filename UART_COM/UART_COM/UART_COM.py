1ab76ba339ad499b8855176554f2d5fa0d5420b8
RexJoker <rexjjoker@gmail.com>
2022-10-05 23:04:03 +0200

    bug fixes, fixed recieving data

--- UART_COM/UART_COM/UART_COM.py
+++ UART_COM/UART_COM/UART_COM.py
@@ -6,20 +6,20 @@ from std_msgs.msg import String
 
 class uart_com():
     def __init__(self):
-        self.port = '/dev/serial1'
+        self.port = '/dev/ttyAMA1'
         self.timeout = 0.1
         # TODO: #1 baudrate = 115200 8n2
         self.uart = serial.Serial(self.port,timeout=self.timeout)
     def recieve(self):
         # gather full line of data through UART
         response = self.uart.readline()
+        str_response = response.decode("utf-8")
         # split response from ending characters \r\n
-        single_msg = response.split('\r\n')
-        # check if data is correct by finding ';'
-        if(single_msg[0].find(';' != -1)):
-            return single_msg[0] # return recieved data
-        else:
-            return "F" # return that something goes wrong
+        if str_response.find('\r\n') != -1:
+            message = str_response.split('\r\n')
+            return str(message[0])
+        return "NULL"
+        return single_msg
     def send(self, arg_msg):
         #try to convert msg to string
         try:
@@ -41,6 +41,9 @@ class uart_ros(Node):
         self.subscriber = self.create_subscription(String, 'UARTTX_topic', self.topic_callback, 10)
         self.publisher = self.create_publisher(String, 'UARTRX_topic', 10)
         self.uart_obj = uart_com()
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
         self.subscriber
     # typical callback function for catching data from topic and sending them directly via UART
     def topic_callback(self, msg):
@@ -48,6 +51,12 @@ class uart_ros(Node):
     # template-temporary feedback callback function which will give data for feedback loop - actions
     def feedback_callback(self):
         self.publisher.publish("rx_feedback")
+    def timer_callback(self):
+        msg = String()
+        msg.data = self.uart_obj.recieve()
+        if msg.data == "NULL":
+            return
+        self.publisher.publish(msg)
     def __del__(self):
         del self.uart_obj
 
