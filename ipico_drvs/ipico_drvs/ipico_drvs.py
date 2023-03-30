28dfb72d39e2bb4fb01d79a6eb269948008bba6b
RexJoker <rexjjoker@gmail.com>
2023-03-30 23:56:12 +0200

    odometry

--- ipico_drvs/ipico_drvs/ipico_drvs.py
+++ ipico_drvs/ipico_drvs/ipico_drvs.py
@@ -3,18 +3,23 @@ from time import sleep
 import time
 from xmlrpc.client import boolean
 import rclpy
+import tf2_ros
+import math
 from rclpy.node import Node
-from geometry_msgs.msg import Twist as BSmart #easter egg
+from tf_transformations import quaternion_from_euler
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3, TransformStamped #easter egg
 from std_msgs.msg import String
+from nav_msgs.msg import Odometry
 import threading
 
 class ipico_node(Node):
     def __init__(self):
         super().__init__('ipico_node')
         #initialize subscriptions and publishing for Ipico_node
-        self.vel_sub = self.create_subscription(BSmart, 'cmd_vel', self.cmd_vel_callback, 1)
+        self.vel_sub = self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_callback, 1)
         self.uart_sub = self.create_subscription(String, 'UARTRX_topic', self.uart_callback, 1)
         self.uart_pub = self.create_publisher(String, 'UARTTX_topic', 10)
+        self.odom_pub = self.create_publisher(Odometry, 'odom', 50)
         self.update = False
         self.velocity = {
             "linear" : {"x":0.0,"y":0.0,"z":0.0},
@@ -23,11 +28,22 @@ class ipico_node(Node):
         self.rx_update = False
         self.vel_update = True
         self.feedback = "None"
+        self.odom_broadcaster = tf2_ros.TransformBroadcaster(self)
+        self.x = 0.0
+        self.y = 0.0
+        self.th = 0.0
+        self.vx = 0.0
+        self.vy = 0.0
+        self.vth = 0.0
+        self.current_time = self.get_clock().now()
+        self.last_time = self.get_clock().now()
+        self.header_stamp = self.current_time.to_msg()
         self.vel_sub
         self.uart_sub
         self.ipico_drvs = ipico_drvs(self)
         self.teleop = teleop_twist(self)
-
+        self.rw = 0.1 # 10 cm in meters // diameter between wheels
+        self.l = 0.2 # 20cm in meter // distance between wheels
     def uart_callback(self, msg):
         self.feedback = msg.data
         self.rx_update = True
@@ -67,6 +83,95 @@ class ipico_node(Node):
         msg = String()
         msg.data = arg_string
         return msg
+    def construct_send_odom(self,wl,wr):
+        self.current_time = self.get_clock().now()
+        dt = (self.current_time - self.last_time ).nanoseconds / 1e9
+        self.y = 0.0
+        self.vx = self.velocity["linear"]["x"]
+        self.vy = 0.0
+        self.vth = self.velocity["angular"]["z"]
+        delta_th = (self.rw/self.l)*(wr-wl)*dt
+        self.th = self.th + delta_th
+        delta_x = math.cos(self.th)*(self.rw/2)*(wr+wl)*dt
+        delta_y = math.sin(self.th)*(self.rw/2)*(wr+wl)*dt
+        self.x = self.x + delta_x
+        self.y = self.y + delta_y
+
+        # # first, we'll publish the transform over tf
+        # self.odom_broadcaster.sendTransform(
+        #     (self.x, self.y, 0.),
+        #     odom_quat,
+        #     self.current_time,
+        #     "base_link",
+        #     "odom"
+        # )
+
+        t = TransformStamped()
+
+        # Read message content and assign it to
+        # corresponding tf variables
+        t.header.stamp = self.current_time.to_msg()
+        t.header.frame_id = 'odom'
+        t.child_frame_id = "base_link"
+
+        # Turtle only exists in 2D, thus we get x and y translation
+        # coordinates from the message and set the z coordinate to 0
+        t.transform.translation.x = float(self.x)
+        t.transform.translation.y = float(self.y)
+        t.transform.translation.z = 0.0
+
+        # For the same reason, turtle can only rotate around one axis
+        # and this why we set rotation in x and y to 0 and obtain
+        # rotation in z axis from the message
+        # since all odometry is 6DOF we'll need a quaternion created from yaw
+        odom_quat = quaternion_from_euler(0, 0, self.th)
+
+        t.transform.rotation.x = odom_quat[0]
+        t.transform.rotation.y = odom_quat[1]
+        t.transform.rotation.z = odom_quat[2]
+        t.transform.rotation.w = odom_quat[3]
+
+        # Send the transformation
+        self.odom_broadcaster.sendTransform(t)
+
+        # next, we'll publish the odometry message over ROS
+        odom = Odometry()
+        odom.header.stamp = self.current_time.to_msg()
+        odom.header.frame_id = "odom"
+        
+        # set the position
+        pt = Point()
+        pt.x = self.x
+        pt.y = self.y
+        pt.z = 0.
+        quat = Quaternion()
+        quat.x = odom_quat[0]
+        quat.y = odom_quat[1]
+        quat.z = odom_quat[2]
+        quat.w = odom_quat[3]
+        pose1 = Pose()
+        pose1.position = pt
+        pose1.orientation = quat
+        odom.pose.pose = pose1
+        vec = Vector3()
+        vec.x = self.vx
+        vec.y = self.vy
+        vec.z = 0.
+        vec2 = Vector3()
+        vec2.x = 0.
+        vec2.y = 0.
+        vec2.z = self.vth
+        twist1 = Twist()
+        twist1.linear = vec
+        twist1.angular = vec2
+        # set the velocity
+        odom.child_frame_id = "base_link"
+        odom.twist.twist = twist1
+
+        # publish the message
+        self.odom_pub.publish(odom)
+
+        self.last_time = self.current_time
 
 class ipico_drvs(threading.Thread):
     class action_type(Enum):
@@ -131,20 +236,20 @@ class ipico_drvs(threading.Thread):
     def drvs_init_procedure(self):
         error_flag = False
         # first check connection with command
-        if not self.check_connection():
-            print("Check connection procedure failed")
-            print("Trying to resolve problem..")
-            self.ros_node.get_logger().info("Check connection procedure failed")
-            self.ros_node.get_logger().info("Trying to resolve problem..")
-            #trying to solve problem by sending some control commands:
-            self.ros_node.uart_pub.publish(self.ros_node.construct_string_msg(self.request_commands["stop"]))
-            self.remember_command("stop")
-            sleep(0.1)
-            self.ros_node.uart_pub.publish(self.ros_node.construct_string_msg(self.request_commands["disable"]))
-            self.remember_command("disable")
-            self.ros_node.rx_update = False
-            if not self.check_connection():
-                raise Exception("Check connection procedure failed")
+        # if not self.check_connection():
+        #     print("Check connection procedure failed")
+        #     print("Trying to resolve problem..")
+        #     self.ros_node.get_logger().info("Check connection procedure failed")
+        #     self.ros_node.get_logger().info("Trying to resolve problem..")
+        #     #trying to solve problem by sending some control commands:
+        #     self.ros_node.uart_pub.publish(self.ros_node.construct_string_msg(self.request_commands["stop"]))
+        #     self.remember_command("stop")
+        #     sleep(0.1)
+        #     self.ros_node.uart_pub.publish(self.ros_node.construct_string_msg(self.request_commands["disable"]))
+        #     self.remember_command("disable")
+        #     self.ros_node.rx_update = False
+        #     if not self.check_connection():
+        #         raise Exception("Check connection procedure failed")
         # second start drivers
         self.ros_node.uart_pub.publish(self.ros_node.construct_string_msg(self.request_commands["fullstep"]))
         self.remember_command("fullstep")
@@ -267,6 +372,12 @@ class teleop_twist(threading.Thread):
         self.reached = True
         self.end = False
         self.ros_node = arg_node
+        self.wl = 0.00
+        self.wr = 0.00
+        self.l = 0.2
+        self.rw = 0.1
+        self.d1 = 0
+        self.d2 = 0
         threading.Thread.__init__(self,daemon=True)
         threading.Thread.start(self)
     #check function for delaying changes into speeds of motors
@@ -346,7 +457,14 @@ class teleop_twist(threading.Thread):
                     if self.check_goal(self.motor["last"], self.motor["actual"], self.motor["goal"]):
                         if (self.velocity["linear"]["x"] == 0) and (self.velocity["angular"]["z"] == 0):
                             self.reached = True
-
+                            self.d1 = 0
+                            self.d2 = 0
+                self.wl = (self.velocity["linear"]["x"]/self.rw) - (self.l * self.velocity["angular"]["z"])/(2*self.rw)
+                self.wr = (self.velocity["linear"]["x"]/self.rw) + (self.l * self.velocity["angular"]["z"])/(2*self.rw)
+                self.ros_node.construct_send_odom(self.wl,self.wr)
+            # self.wl = (self.velocity["linear"]["x"]/self.rw) - (self.l * self.velocity["angular"]["z"])/(2*self.rw) * self.d1
+            # self.wr = (self.velocity["linear"]["x"]/self.rw) + (self.l * self.velocity["angular"]["z"])/(2*self.rw) * self.d2
+            # self.ros_node.construct_send_odom(self.wl,self.wr)
             #send data through uart by ipico drivers
             if self.end:
                 return
@@ -356,15 +474,20 @@ class teleop_twist(threading.Thread):
             "M1" : 300,
             "M2" : 300
         }
+        self.d1 = 1
+        self.d2 = 1
         if self.motor["request"]["M1"] < 0:
             steps["M1"] = -300
+            self.d1 = -1
         elif self.motor["request"]["M1"] == 0:
             steps["M1"] = 0
-        
+            self.d1 = 0
         if self.motor["request"]["M2"] < 0:
             steps["M2"] = -300
+            self.d2 = -1
         elif self.motor["request"]["M2"] == 0:
             steps["M2"] = 0
+            self.d2 = 0
 
         if steps["M1"] != 0:
             self.ros_node.ipico_drvs.move_command(move_type=self.ros_node.ipico_drvs.move_type.step.value, action_type=self.ros_node.ipico_drvs.action_type.set.value, driver_nr=1,value=steps["M1"])
